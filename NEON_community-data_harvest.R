78c65629a894d45562ecbe0f38d1a707f72997f1
bryansmclean <8601578+bryansmclean@users.noreply.github.com>
2022-02-18 11:09:56 -0500

    Add files via upload
    add tally of total trap nights (=effort)

--- NEON_community-data_harvest.R
+++ NEON_community-data_harvest.R
@@ -17,18 +17,23 @@ setwd('/Users/mclean/Box/Projects/FuTRES/traits-v-space/_data_/NEON_count-small-
 top.dir <- getwd()
 data.dirs <- list.dirs()
 data.dirs <- data.dirs[grepl('expanded', data.dirs, ignore.case = T, fixed = F)]
-data.dirs <- data.dirs[grep('GRSM|ORNL|MLBS|BLAN|SCBI|BART|HARV', data.dirs)] # just the Appalachians sites
+sites <- 'GRSM|ORNL|MLBS|BLAN|SCBI|BART|HARV' # list of just the Appalachians sites
+data.dirs <- data.dirs[grep(sites, data.dirs)]
+Total_traps <- rep(0, length(strsplit(sites, '|', fixed = T)[[1]])); names(Total_traps) <- strsplit(sites, '|', fixed = T)[[1]] # data frame of the total trap effort, per site
 
 # loop through files to build full record-set
 for(i in data.dirs){
   
-  # read file-wise retaining smammal captures
+  # read file-wise retaining desired info
   setwd(i)
   data.file <- list.files(pattern = 'pertrapnight')
   tmp <- fread(data.file)
-  caps <- tmp[grep("4 -|5 -", tmp$trapStatus), ] # all captures
-  indivs <- caps[which(caps$tagID != ""), ] # all captures with a unique identifier
+  caps <- tmp[grep("4 -|5 -", tmp$trapStatus), ] # data frame of all captures
+  indivs <- caps[which(caps$tagID != ""), ] # data frame of all captures with a unique identifier
   
+  # record the total trapping effort
+  Total_traps[unique(tmp$siteID)] <- Total_traps[unique(tmp$siteID)] + nrow(tmp)
+
   # append all captures to file
   if(i == data.dirs[1]){
     caps.all <- caps
@@ -89,8 +94,11 @@ for(i in sites){
 
   }
 
-write.csv(cap.counts, '/Users/mclean/Box/Projects/GI_smammal_community-traits/_data_/NEON_Appalachians-smammals_total-captures.csv', row.names = T, quote = F)
-write.csv(indiv.counts, '/Users/mclean/Box/Projects/GI_smammal_community-traits/_data_/NEON_Appalachians-smammals_total-individuals.csv', row.names = T, quote = F)
+cap.counts <- rbind(cap.counts, Total_traps = Total_traps[colnames(cap.counts)])
+indiv.counts <- rbind(indiv.counts, Total_traps = Total_traps[colnames(indiv.counts)])
+
+write.csv(cap.counts, '/Users/mclean/Box/Projects/GI_smammal-community-traits/_data_/NEON_Appalachians-smammals_total-captures.csv', row.names = T, quote = F)
+write.csv(indiv.counts, '/Users/mclean/Box/Projects/GI_smammal-community-traits/_data_/NEON_Appalachians-smammals_total-individuals.csv', row.names = T, quote = F)
 
 
 
